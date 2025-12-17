6050d4ac5a072de02d6ef736829ae1e69213cb2b
Pawan-techo <pawanbhuyar333@gmail.com>
2025-12-17 15:32:21 +0530

    Home section error fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import MainCarousel from "../components/Customer/HomeCorousel/MainCarousel.jsx"
 import HomeSectionCards from '../components/Customer/HomeSectionCard/HomeSectionCard.jsx';
-import HomeSectionCards2 from '../components/Customer/HomeSectionCard/HomeSectionCard2.jsx';
 import TwoSideCard from '../components/Customer/HomeSectionCard/TwoSideCard.jsx';
 import OurFeatures from '../components/Customer/HomeSectionCard/Ourfeatures.jsx';
 import Categories from '../components/Customer/HomeSectionCard/categories.jsx';
+
 const Home = () => {
   return (
     <div>
